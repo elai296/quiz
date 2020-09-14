d144c6623b970085d1d21d7010da60347dd4ccb9
Elaine Phan <elainep926@gmail.com>
2020-09-13 17:02:17 -0700

    added result

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -12,6 +13,24 @@ form.addEventListener('submit', e => {
         if(answer === correctAnswers[index]){
             score += 25;
         }
+        scrollTo(0, 0);
+        result.querySelector('span').textContent = `${score}%`;
+        result.classList.remove('d-none');
     });
     console.log(score);
 });
+
+//window object(global object)
+
+//console.log('hello');
+//window.console.log('hello'); //same thing as above line, 
+
+//console.log(document.querySelector('form'));
+//console.log(window.document.querySelector('.form')); same thing as above line, document is an object in the window object
+
+// alert('hello');
+// window.alert('hello'); same thing as above line
+
+// setTimeout(() => {
+//     alert('hello, ninjas'); //it will wait 3 seconds then it will run the alert function
+// }, 3000);
